c2866c39529204d4caef8669bff699ff20b35d16
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2020-02-16 11:36:28 +0000

    Validation of data received from /register and /signin endpoints

--- controllers/signin.js
+++ controllers/signin.js
@@ -1,12 +1,16 @@
 const handleSignIn = (req, res, db, bcrypt) => {
+	const { email, password } = req.body;
+	if (!email || !password) {
+		return res.status(400).json('invalid form data');
+	}
 	db.select('email', 'hash').from('login')
-		.where('email', '=', req.body.email)
+		.where('email', '=', email)
 		.then(loginData => {
-			const isValid = bcrypt.compareSync(req.body.password, loginData[0].hash);
+			const isValid = bcrypt.compareSync(password, loginData[0].hash);
 			if (isValid) {
 				// return user
 				db.select('*').from('users')
-					.where('email', '=', req.body.email)
+					.where('email', '=', email)
 					.then(user => res.json(user[0]))
 				;
 			} else {
