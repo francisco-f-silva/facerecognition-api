638ce348fc983b133d7e5329da5d0772129b0b81
Francisco Silva <franciscocpfsilva_fcp@hotmail.com>
2020-02-16 12:25:01 +0000

    Passed Clarifai API call to the backend (to hide API key)

--- controllers/image.js
+++ controllers/image.js
@@ -1,3 +1,17 @@
+const Clarifai = require('clarifai');
+
+const app = new Clarifai.App({
+ apiKey: '39c7f5269c8f40d38ab3a96dc546d3a7'
+});
+
+const handleApiCall = (req, res) => {
+	app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+      .then(data => res.json(data))
+      .catch(err => res.status(400).json('unable to work with API'))
+	;
+}
+
 const handleImage = (req, res, db) => {
 	const { id } = req.body;
 	// UPDATE users SET entries = entries + 1 WHERE id = ...;
@@ -18,5 +32,6 @@ const handleImage = (req, res, db) => {
 };
 
 module.exports = {
-	handleImage: handleImage
+	handleImage: handleImage,
+	handleApiCall: handleApiCall
 };
